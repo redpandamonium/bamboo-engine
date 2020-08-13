e5d569f077d26ce4c3bf7c4b9eca8d097f39c49c
Leon Suchy <39390799+redpandamonium@users.noreply.github.com>
2020-08-13 12:13:24 +0200

    Added more progress towards a simple rendering pipeline

--- src/bamboo_engine/graphics/vulkan_pipeline.cpp
+++ src/bamboo_engine/graphics/vulkan_pipeline.cpp
@@ -23,16 +23,43 @@
 
 namespace bbge {
 
-    std::array<std::string_view, enum_size + 1> shader_type_names {
+    constexpr std::array<std::string_view, shader_type::enum_size + 1> shader_type_names {
         "fragment", "vertex", "geometry", "compute", "INVALID_SHADER_TYPE"
     };
 
-    const std::string_view& to_string(shader_type t) {
+    std::string_view to_string(shader_type t) {
         if (t >= shader_type::enum_size) return shader_type_names[shader_type::enum_size];
         return shader_type_names[t];
     }
 
-    std::array<VkShaderStageFlagBits, enum_size + 1> shader_type_vk_conversion {
+    constexpr std::array<std::string_view, 3> rasterizer_mode_names {
+        "fill", "line", "point"
+    };
+
+    std::string_view to_string(rasterizer_mode m) {
+        auto v = static_cast<uint8_t>(m);
+        return rasterizer_mode_names[v];
+    }
+
+    constexpr std::array<std::string_view, 4> rasterizer_cull_mode_names {
+        "none", "front", "back", "both"
+    };
+
+    std::string_view to_string(rasterizer_cull_mode m) {
+        auto v = static_cast<uint8_t>(m);
+        return rasterizer_cull_mode_names[v];
+    }
+
+    constexpr std::array<std::string_view, 2> rasterizer_front_face_names {
+        "clockwise", "counter-clockwise"
+    };
+
+    std::string_view to_string(rasterizer_front_face m) {
+        auto v = static_cast<uint8_t>(m);
+        return rasterizer_front_face_names[v];
+    }
+
+    constexpr std::array<VkShaderStageFlagBits, shader_type::enum_size + 1> shader_type_vk_conversion {
         VK_SHADER_STAGE_FRAGMENT_BIT, VK_SHADER_STAGE_VERTEX_BIT, VK_SHADER_STAGE_GEOMETRY_BIT, VK_SHADER_STAGE_COMPUTE_BIT
     };
 
@@ -41,6 +68,39 @@ namespace bbge {
             throw std::logic_error("Vulkan does not support the requested shader type.");
         }
         return shader_type_vk_conversion[t];
+    }
+
+    constexpr std::array<VkPolygonMode, 3> polygon_mode_vk_conversion {
+        VkPolygonMode::VK_POLYGON_MODE_FILL,
+        VkPolygonMode::VK_POLYGON_MODE_LINE,
+        VkPolygonMode::VK_POLYGON_MODE_POINT
+    };
+
+    VkPolygonMode to_vulkan(rasterizer_mode m) {
+        auto v = static_cast<uint8_t>(m);
+        return polygon_mode_vk_conversion[v];
+    }
+
+    constexpr std::array<VkCullModeFlagBits, 4> cull_mode_vk_conversion {
+        VkCullModeFlagBits::VK_CULL_MODE_NONE,
+        VkCullModeFlagBits::VK_CULL_MODE_FRONT_BIT,
+        VkCullModeFlagBits::VK_CULL_MODE_BACK_BIT,
+        VkCullModeFlagBits::VK_CULL_MODE_FRONT_AND_BACK
+    };
+
+    VkCullModeFlags to_vulkan(rasterizer_cull_mode m) {
+        auto v = static_cast<uint8_t>(m);
+        return cull_mode_vk_conversion[v];
+    }
+
+    constexpr std::array<VkFrontFace, 2> front_face_vk_conversion {
+        VkFrontFace::VK_FRONT_FACE_CLOCKWISE,
+        VkFrontFace::VK_FRONT_FACE_COUNTER_CLOCKWISE
+    };
+
+    VkFrontFace to_vulkan(rasterizer_front_face f) {
+        auto v = static_cast<uint8_t>(f);
+        return front_face_vk_conversion[v];
     }
 
     result<std::vector<std::byte>, std::runtime_error> vulkan_pipeline::load_binary_file(const std::filesystem::path& p) {
@@ -81,7 +141,9 @@ namespace bbge {
         std::string&& name, const vulkan_pipeline::shader_module_paths& module_paths,
         const rendering_pipeline_settings& settings,
         VkDevice dev)
-      : m_name(std::move(name)) {
+      : m_name(std::move(name)), m_device(dev), m_layout(VK_NULL_HANDLE) {
+
+        assert(dev);
 
         auto frag_code = load_binary_file(module_paths.fragment_shader).or_throw();
         auto vert_code = load_binary_file(module_paths.vertex_shader).or_throw();
@@ -103,7 +165,103 @@ namespace bbge {
             make_shader_stage_create_info(frag_module.module, shader_type::fragment)
         };
 
-        // TODO: continue here with fixed functions, state descriptors and render passes.
+        // Empty for now. Will have buffer layout descriptors
+        VkPipelineVertexInputStateCreateInfo vertex_input_info { };
+        vertex_input_info.sType                             = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        vertex_input_info.vertexBindingDescriptionCount     = 0;
+        vertex_input_info.pVertexBindingDescriptions        = nullptr; // Optional
+        vertex_input_info.vertexAttributeDescriptionCount   = 0;
+        vertex_input_info.pVertexAttributeDescriptions      = nullptr; // Optional
+
+        // Triangle list
+        VkPipelineInputAssemblyStateCreateInfo input_assembly { };
+        input_assembly.sType                    = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        input_assembly.topology                 = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        input_assembly.primitiveRestartEnable   = VK_FALSE;
+
+        // Viewport & scissor
+        VkViewport viewport = to_vulkan_viewport(settings.viewport);
+        VkRect2D scissor = {
+            { static_cast<int32_t>(viewport.x), static_cast<int32_t>(viewport.y) },
+            { static_cast<uint32_t>(viewport.width), static_cast<uint32_t>(viewport.height) }
+        };
+
+        // There is only one scissor and one viewport, so set the above
+        VkPipelineViewportStateCreateInfo viewport_create_info { };
+        viewport_create_info.sType          = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        viewport_create_info.viewportCount  = 1;
+        viewport_create_info.pViewports     = &viewport;
+        viewport_create_info.scissorCount   = 1;
+        viewport_create_info.pScissors      = &scissor;
+
+        // rasterizer
+        VkPipelineRasterizationStateCreateInfo rasterizer_create_info { };
+        rasterizer_create_info.sType            = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        rasterizer_create_info.depthClampEnable = settings.depth_clamp;
+        rasterizer_create_info.polygonMode      = to_vulkan(settings.rasterizer_mode);
+        rasterizer_create_info.lineWidth        = settings.render_width;
+        rasterizer_create_info.cullMode         = to_vulkan(settings.cull_mode);
+        rasterizer_create_info.frontFace        = to_vulkan(settings.front_face);
+        rasterizer_create_info.depthBiasEnable  = settings.depth_bias.has_value();
+        if (settings.depth_bias) {
+            rasterizer_create_info.depthBiasConstantFactor  = settings.depth_bias->const_factor;
+            rasterizer_create_info.depthBiasClamp           = settings.depth_bias->clamp;
+            rasterizer_create_info.depthBiasSlopeFactor     = settings.depth_bias->slope_factor;
+        }
+
+        // multisampling, empty for now
+        VkPipelineMultisampleStateCreateInfo multisampling { };
+        multisampling.sType                 = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multisampling.sampleShadingEnable   = VK_FALSE;
+        multisampling.rasterizationSamples  = VK_SAMPLE_COUNT_1_BIT;
+        multisampling.minSampleShading      = 1.0f;
+        multisampling.pSampleMask           = nullptr;
+        multisampling.alphaToCoverageEnable = VK_FALSE;
+        multisampling.alphaToOneEnable      = VK_FALSE;
+
+        #pragma clang diagnostic push
+        #pragma ide diagnostic ignored "hicpp-signed-bitwise"
+        static constexpr auto all_colors_components =
+            VK_COLOR_COMPONENT_R_BIT |
+            VK_COLOR_COMPONENT_G_BIT |
+            VK_COLOR_COMPONENT_B_BIT |
+            VK_COLOR_COMPONENT_A_BIT;
+        #pragma clang diagnostic pop
+
+        // color blending
+        VkPipelineColorBlendAttachmentState color_blend_attachment { };
+        color_blend_attachment.colorWriteMask       = all_colors_components;
+        color_blend_attachment.blendEnable          = VK_TRUE;
+        color_blend_attachment.srcColorBlendFactor  = VK_BLEND_FACTOR_SRC_ALPHA;
+        color_blend_attachment.dstColorBlendFactor  = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+        color_blend_attachment.colorBlendOp         = VK_BLEND_OP_ADD;
+        color_blend_attachment.srcAlphaBlendFactor  = VK_BLEND_FACTOR_ONE;
+        color_blend_attachment.dstAlphaBlendFactor  = VK_BLEND_FACTOR_ZERO;
+        color_blend_attachment.alphaBlendOp         = VK_BLEND_OP_ADD;
+
+        VkPipelineColorBlendStateCreateInfo color_blending { };
+        color_blending.sType             = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        color_blending.logicOpEnable     = VK_FALSE;
+        color_blending.logicOp           = VK_LOGIC_OP_COPY;
+        color_blending.attachmentCount   = 1;
+        color_blending.pAttachments      = &color_blend_attachment;
+        color_blending.blendConstants[0] = 0.0f;
+        color_blending.blendConstants[1] = 0.0f;
+        color_blending.blendConstants[2] = 0.0f;
+        color_blending.blendConstants[3] = 0.0f;
+
+        // depth and stencil testing setup here
+
+        // pipeline layout, for uniform variables
+        VkPipelineLayoutCreateInfo pipeline_layout_create_info { };
+        pipeline_layout_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+
+        auto res = vkCreatePipelineLayout(dev, &pipeline_layout_create_info, nullptr, &m_layout);
+        if (res != VkResult::VK_SUCCESS) {
+            throw vulkan_error("Failed to create Vulkan pipeline layout", res);
+        }
+
+        // TODO: render passes.
     }
 
     VkPipelineShaderStageCreateInfo
@@ -114,5 +272,11 @@ namespace bbge {
         create_info.module = module;
         create_info.pName = m_name.c_str();
         return create_info;
+    }
+
+    vulkan_pipeline::~vulkan_pipeline() {
+        if (m_layout) {
+            vkDestroyPipelineLayout(m_device, m_layout, nullptr);
+        }
     }
 }
--- src/bamboo_engine/graphics/vulkan_pipeline.hpp
+++ src/bamboo_engine/graphics/vulkan_pipeline.hpp
@@ -26,19 +26,62 @@
 #include <vulkan/vulkan.h>
 #include "vulkan.hpp"
 #include "../util/result.hpp"
+#include "../util/rectangle.hpp"
 
 namespace bbge {
 
-    struct rendering_pipeline_settings {
+    enum class rasterizer_mode : uint8_t {
+        fill, line, point
+    };
 
+    enum class rasterizer_cull_mode : uint8_t {
+        none, front, back, both
+    };
+
+    enum class rasterizer_front_face : uint8_t {
+        clockwise, counter_clockwise
+    };
+
+    // TODO: documentation
+    struct rasterizer_depth_bias {
+        float const_factor;
+        float clamp;
+        float slope_factor;
     };
 
     enum shader_type {
         fragment, vertex, geometry, compute, enum_size
     };
 
-    const std::string_view& to_string(shader_type t);
-    VkShaderStageFlagBits to_vulkan(shader_type t);
+    struct rendering_pipeline_settings {
+
+        // input layout description
+
+        // rasterizer
+        drect                                viewport;                                           // Viewport, should probably fit the window dimensions
+        rasterizer_mode                      rasterizer_mode = rasterizer_mode::fill;            // Rasterizer mode
+        float                                render_width    = 1.0f;                             // Rasterizer point/line width
+        rasterizer_cull_mode                 cull_mode       = rasterizer_cull_mode::back;       // Which side of a primitive should be culled
+        rasterizer_front_face                front_face      = rasterizer_front_face::clockwise; // Which side of the primitive is the front
+        bool                                 depth_clamp     = false;                            // If true the rasterizer will clamp the depth values of fragments outside the clipping planes
+        std::optional<rasterizer_depth_bias> depth_bias      = { };                              // Allows to set the depth bias
+
+        // multisampling
+
+        // color blending
+    };
+
+    // vulkan conversions
+    VkPolygonMode           to_vulkan(rasterizer_mode m);
+    VkCullModeFlags         to_vulkan(rasterizer_cull_mode m);
+    VkFrontFace             to_vulkan(rasterizer_front_face f);
+    VkShaderStageFlagBits   to_vulkan(shader_type t);
+
+    // string conversions
+    std::string_view to_string(shader_type t);
+    std::string_view to_string(rasterizer_mode m);
+    std::string_view to_string(rasterizer_cull_mode m);
+    std::string_view to_string(rasterizer_front_face m);
 
     class vulkan_pipeline {
     public:
@@ -57,9 +100,13 @@ namespace bbge {
             VkDevice dev
         );
 
+        ~vulkan_pipeline();
+
     private:
 
         std::string m_name;
+        VkDevice m_device;
+        VkPipelineLayout m_layout;
 
         [[nodiscard]] static result<std::vector<std::byte>, std::runtime_error> load_binary_file(const std::filesystem::path& p);
         [[nodiscard]] result<VkShaderModule, vulkan_error> create_shader_module(VkDevice dev, const std::vector<std::byte>& spir_v_bytecode, shader_type type) const;
